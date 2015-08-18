ad9e3b41aff05f9539f96a39172ac70298a34857
intern <intern@Sonicmoov-no-MacBook-Air.local>
2015-08-18 11:25:34 +0900

    Initial Commit

---
+++ rakutenViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rakutenViewer
+//
+//  Created by  intern on 2015/08/18.
+//  Copyright (c) 2015年 sonicmoov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
