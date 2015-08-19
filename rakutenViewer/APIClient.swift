238e3b476adac2d03452ad262cc2082f86df6091
intern <intern@Sonicmoov-no-MacBook-Air.local>
2015-08-19 10:57:33 +0900

    first commit

---
+++ rakutenViewer/APIClient.swift
@@ -0,0 +1,13 @@
+//
+//  APIClient.swift
+//  rakutenViewer
+//
+//  Created by  intern on 2015/08/18.
+//  Copyright (c) 2015年 sonicmoov. All rights reserved.
+//
+
+import UIKit
+
+class APIClient: NSObject {
+   
+}
---
+++ rakutenViewer/SearchManager.swift
@@ -0,0 +1,9 @@
+//
+//  manager.swift
+//  rakutenViewer
+//
+//  Created by  intern on 2015/08/18.
+//  Copyright (c) 2015年 sonicmoov. All rights reserved.
+//
+
+import Foundation
